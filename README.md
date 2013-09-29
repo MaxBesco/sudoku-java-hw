9902597a4f8ee1ebc1c8a5127c79f8e5898747e7
John Foley <jfoley@cs.umass.edu>
2013-09-29 08:43:54 -0400

    C++ -> Java

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 sudoku-cc
 =========
 
-Sudoku Solver written in C++11 for AI homework.
+Sudoku Solver written in Java for AI homework.
 CS683, Fall 2013, University of Massachusetts, School of Computer Science.
---
+++ src/main/java/sudoku/Tokenizer.java
@@ -0,0 +1,15 @@
+package sudoku;
+
+class Tokenizer {
+
+  public static int[] tokenize(String filePath) {
+    int output[] = new int[81];
+    
+    return output;
+  };
+  
+  public static void main(String[] args) {
+    System.out.println("Tokenizer.main");
+  }
+}
+
